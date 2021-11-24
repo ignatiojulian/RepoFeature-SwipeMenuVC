6ab3fa40062770fe19c05cfba4bf687ccd3048fb
Ignatio <ignatio.tara@multipolar.com>
2021-11-24 13:55:10 +0800

    initial project

---
+++ SwipeMenuVC/SwipeMenuVC/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwipeMenuVC
+//
+//  Created by Ignatio Julian on 24/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

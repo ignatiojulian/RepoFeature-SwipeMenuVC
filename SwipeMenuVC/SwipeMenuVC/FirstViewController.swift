64b0b14e0cb090077526840a011fb7ff1ea3d5ce
Ignatio <ignatio.tara@multipolar.com>
2021-11-24 16:00:09 +0800

    add swipe menu feature

---
+++ SwipeMenuVC/SwipeMenuVC/FirstViewController.swift
@@ -0,0 +1,37 @@
+//
+//  FirstViewController.swift
+//  SwipeMenuVC
+//
+//  Created by Ignatio Julian on 24/11/21.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func printAction(_ sender: UIButton) {
+        print("FirstViewController")
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ SwipeMenuVC/SwipeMenuVC/ThirdViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ThirdViewController.swift
+//  SwipeMenuVC
+//
+//  Created by Ignatio Julian on 24/11/21.
+//
+
+import UIKit
+
+class ThirdViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func printAction(_ sender: UIButton) {
+        print("ThirdViewController")
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
